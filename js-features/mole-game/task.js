509bc77d15124cd65ccdc0ac341992042568fa1f
Пётр Фирсов <peterfirsoff@gmail.com>
2024-02-11 16:22:17 +0300

    Четвёртый вариант

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,18 +2,6 @@ let deadMoles = document.getElementById('dead');
 let misses = document.getElementById('lost');
 
 clickCount = () => {
-    let hole1 = document.getElementById('hole1');
-    let hole2 = document.getElementById('hole2');
-    let hole3 = document.getElementById('hole3');
-    let hole4 = document.getElementById('hole4');
-    let hole5 = document.getElementById('hole5');
-    let hole6 = document.getElementById('hole6');
-    let hole7 = document.getElementById('hole7');
-    let hole8 = document.getElementById('hole8');
-    let hole9 = document.getElementById('hole9');
-
-    let holes = [hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9];
-
     if (Number(deadMoles.textContent) === 10) {
         alert('Победа!')
     }
@@ -22,9 +10,16 @@ clickCount = () => {
         alert('Вы проиграли!')
     }
 
-    if (holes[i].onclick && holes[i].className.contains('hole_has-mole')) {
-        deadMoles.textContent += 1;
-    } else {
-        misses.textContent += 1;
+    let holeNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (let i = 0; i < holeNumbers.length; i++) {
+        if (holeNumber[i].onclick) {
+            let element = document.getElementById(`hole${i}`);
+            if (element.className.contains('hole_has-mole')) {
+                deadMoles.textContent += 1;
+            } else {
+                misses.textContent += 1;
+            }
+        }
     }
+
 }
