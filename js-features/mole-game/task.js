42ffe589c259de1448736cd2d7633e72c3594c01
Пётр Фирсов <peterfirsoff@gmail.com>
2024-02-11 16:04:16 +0300

    Третий вариант

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -2,7 +2,17 @@ let deadMoles = document.getElementById('dead');
 let misses = document.getElementById('lost');
 
 clickCount = () => {
-    let hole = index => document.getElementById(`hole${index}`);
+    let hole1 = document.getElementById('hole1');
+    let hole2 = document.getElementById('hole2');
+    let hole3 = document.getElementById('hole3');
+    let hole4 = document.getElementById('hole4');
+    let hole5 = document.getElementById('hole5');
+    let hole6 = document.getElementById('hole6');
+    let hole7 = document.getElementById('hole7');
+    let hole8 = document.getElementById('hole8');
+    let hole9 = document.getElementById('hole9');
+
+    let holes = [hole1, hole2, hole3, hole4, hole5, hole6, hole7, hole8, hole9];
 
     if (Number(deadMoles.textContent) === 10) {
         alert('Победа!')
@@ -12,7 +22,7 @@ clickCount = () => {
         alert('Вы проиграли!')
     }
 
-    if (hole.onclick && hole.className.contains('hole_has-mole')) {
+    if (holes[i].onclick && holes[i].className.contains('hole_has-mole')) {
         deadMoles.textContent += 1;
     } else {
         misses.textContent += 1;
