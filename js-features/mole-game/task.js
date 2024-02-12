0d77bb3a9dc30b02e78bf95e0714bd55ade411d8
Пётр Фирсов <peterfirsoff@gmail.com>
2024-02-12 12:14:04 +0300

    Шестая версия

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -10,17 +10,16 @@ clickCount = () => {
         alert('Вы проиграли!')
     }
 
-    let holeNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    for (let i = 0; i < holeNumber.length; i++) {
-        if (document.getElementById(`hole${i}`).onclick) {
-            let element = document.getElementById(`hole${i}`);
-            if (element.className.contains('hole_has-mole')) {
-                deadMoles.textContent += 1;
-            } else {
-                misses.textContent += 1;
-            }
+    const getHole = index => document.getElementById(`hole${index}`);
+
+    for (let index = 1; index <= 9; index++) {
+        let hole = getHole(index);
+        hole.onclick = function() {
+            if (hole.className.includes('hole_has-mole')) {
+                    deadMoles.textContent += 1;
+                } else {
+                    misses.textContent += 1;
+                }
         }
     }
-    // Проверять лунки с номерами из массива на нажатие.
-    // При нажатии обратиться к элементу по id 'лунка[номер нажатой лунки]'
 }
